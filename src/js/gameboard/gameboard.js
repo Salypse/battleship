e00ab46bc4cf58736f6e878dfd9245fe06c786c0
Salypse <84153483+Salypse@users.noreply.github.com>
2025-12-05 14:02:01 -0600

    Create findCells functions that returns the cells that the given ship can be placed on with the given coordinate

--- src/js/gameboard/gameboard.js
+++ src/js/gameboard/gameboard.js
@@ -7,5 +7,32 @@ export class Gameboard {
             attacked: false,
          }))
       );
+
+      this.rotateShip = false;
+   }
+
+   placeShip(coordinate, ship) {
+      //Find cells ship will be placed on
+      const cellsToPlace = findCells(coordinate, ship.length);
+
+      //Check if ship when placed will be out of bounds
+      //Check if any of the cells already have a ship
+      //Place ship in gameboard
+   }
+
+   findCells(coordinate, length) {
+      let x = coordinate[0];
+      let y = coordinate[1];
+
+      let cells = [[x, y]];
+
+      while (cells.length < length) {
+         if (this.rotateShip === true) {
+            cells.push([x, ++y]);
+         } else {
+            cells.push([++x, y]);
+         }
+      }
+      return cells;
    }
 }
--- src/js/gameboard/gameboard.test.js
+++ src/js/gameboard/gameboard.test.js
@@ -19,3 +19,24 @@ test("Every grid cell has correct default values", () => {
       )
    );
 });
+
+test("Find correct cells for given coordinate", () => {
+   const board = new Gameboard();
+
+   //No rotate
+   expect(board.findCells([5, 5], 3)).toEqual([
+      [5, 5],
+      [6, 5],
+      [7, 5],
+   ]);
+
+   //Rotate
+   board.rotateShip = true;
+
+   expect(board.findCells([3, 2], 4)).toEqual([
+      [3, 2],
+      [3, 3],
+      [3, 4],
+      [3, 5],
+   ]);
+});
