9d160529301d326eecbd81d9ac98c03388efdfe9
Salypse <84153483+Salypse@users.noreply.github.com>
2025-12-10 11:52:04 -0600

    Create home page with options for game mode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import "./styles.css"
+import "./styles.css";
+
+const pvpButton = document.getElementById("pvp");
+const pvcButton = document.getElementById("pvc");
+
+pvpButton.addEventListener("click", () => {
+   const body = document.querySelector("body");
+   body.innerHTML = "";
+
+   //Start pvp game function
+});
+
+pvcButton.addEventListener("click", () => {
+   const body = document.querySelector("body");
+   body.innerHTML = "";
+
+   //Start pvc game function
+});
